eaabdcd7abaf74da7cc38d6c42087c646fa3d914
rene-INTech <remi.lamiaux@telecom-sudparis.eu>
2019-05-25 10:09:18 +0200

    modif de l'echo

--- LED/LED.py
+++ LED/LED.py
@@ -24,7 +24,7 @@ connection.listen(5)
 
 sgl.signal(sgl.SIGTERM, terminate)
 sgl.signal(sgl.SIGUSR1, terminate)
-os.system("echo " + str(os.getpid()) + "\n >/home/pi/panneauRaspi/LED/PID")
+os.system('sudo echo "' + str(os.getpid()) + '"\n >/home/pi/panneauRaspi/LED/PID')
 
 while True:
 	client, info = connection.accept()
